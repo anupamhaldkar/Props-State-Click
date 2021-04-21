c536553b42fda70bc861a32e788a1ae79dfec1af
anupamhaldkar <ahdev2020@outlook.com>
2021-04-21 10:53:12 +0530

    Project Rendering by myself

---
+++ src/Application.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Application extends Component {
+    render(){
+        return (
+            <div>
+            <h1>Hello, World</h1>
+            <span>Hmm</span>
+            </div>
+        );
+    }
+}
+export default Application;
